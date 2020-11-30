79dccd7a07435e5c9562f8d2395903ad6f9986f8
la4zen <65774126+la4zen@users.noreply.github.com>
2020-11-30 18:45:39 +0300

    Register added

--- back/main.py
+++ back/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, Request, Response
-import sqlite3
-from util import dict_factory
+from flask import Flask, request
+import sqlite3, re
+from util import *
+from hashlib import md5
 
 app = Flask(__name__)
 
@@ -16,6 +17,17 @@ c.execute("""
         text TEXT,
         date DATETIME DEFAULT CURRENT_TIMESTAMP
     );
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   
+        user_name TEXT,
+        password TEXT,
+        regtime DATETIME DEFAULT CURRENT_TIMESTAMP
+    ); 
+    CREATE TABLE IF NOT EXISTS sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        token TEXT
+    );
 """)
 db.commit()
 
@@ -24,10 +36,35 @@ def getPosts():
     c.execute("SELECT * FROM posts")
     return {"response" : c.fetchall()}
 
-@app.route("/api/getPost<id>")
+@app.route("/api/getPost_<id>")
 def getPost(id):
     c.execute(f"SELECT * FROM posts WHERE id={id}")
-    return {"response":c.fetchone()}
+    result = c.fetchone()
+    return {"response" : result} if result != None else {"error" : "id not found"}
+
+@app.route("/api/login")
+def login():
+    return {"response" : 200}
+
+@app.route("/api/register")
+def register():
+    login, password = request.data.get("login"), request.data.get("password")
+    r = r"[A-Za-z]"
+    if any([login, password]) == None:
+        return {"error" : "Необходимо указать логин и пароль"}
+    elif all([len(login) < 6, len(password) < 8]):
+        return {"error" : "Длина логина должна быть больше 6-ти символов, а пароль должен быть больше 8-ми символов"}
+    elif all([re.sub(r, "", login), re.sub(r, "", password)]):
+        return {"error" : "В логине или пароле не должны использоваться спец-символы"}
+    else:
+        c.execute(f"SELECT * FROM users WHERE login='{login}'")
+        if c.fetchone():
+            return {"error" : "Пользователь с таким логином уже существует."}
+        else:
+            tkn = randstr()
+            lastrowid = c.execute(f"INSERT INTO users(user_name, password) VALUES ('{login}', '{md5(password)}')").lastrowid
+            c.execute(f"INSERT INTO sessions(user_id, token) VALUES ({lastrowid}, '{tkn}')")
+            return {"response" : 200, "tkn" : tkn}
 
 # main router
 app.run()
--- back/util.py
+++ back/util.py
@@ -1,5 +1,11 @@
+from random import choice
+from string import ascii_letters, digits
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
+
+def randstr(lenght:int=32):
+    return "".join(choice(ascii_letters+digits) for in range(lenght))
